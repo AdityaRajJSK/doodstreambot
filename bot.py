a8d3edf047f0b2e957492d7cf494a5b0706fbeca
AdityaRajJSK <73116892+AdityaRajJSK@users.noreply.github.com>
2023-05-17 20:11:23 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -239,15 +239,33 @@ def handle_private(message,chatid,msgid):
 		if os.path.exists(f'{message.chat.id}{message.message_id}upstatus.txt'): os.remove(f'{message.chat.id}{message.message_id}upstatus.txt')
 
 
-@bot.on_message(filters.video)
+@bot.on_message(filters.video | filters.document)
 async def vdood_upload(bot, message):
     smsg = bot.send_message(message.chat.id, '__Downloading__', reply_to_message_id=message.message_id)
-    dosta = threading.Thread(target=lambda:downstatus(f'{message.chat.id}{message.message_id}downstatus.txt',smsg),daemon=True)
-    dosta.start()
-    file = bot.download_media(message, progress=progress, progress_args=[message,"down"])
+    """dosta = threading.Thread(target=lambda:downstatus(f'{message.chat.id}{message.message_id}downstatus.txt',smsg),daemon=True)
+    dosta.start()"""
+    await file = bot.download_media(message, progress=progress, progress_args=[message,"down"])
     #os.remove(f'{message.chat.id}{message.message_id}downstatus.txt')
     """upsta = threading.Thread(target=lambda:upstatus(f'{message.chat.id}{message.message_id}upstatus.txt',smsg),daemon=True)
     upsta.start()"""
+    path=file
+		if "Document" or "Video" in str(msg):
+			try:
+				u = d.local_upload(path)
+				print("#" * 10 + " Local Upload " + "#" * 10)
+				print(f"Status : {u['status']}")
+				print(f"Video ID : {u['result'][0]['filecode']}")
+				print(f"Video Url : {u['result'][0]['download_url']}")
+				print(f"Protected DL : {u['result'][0]['protected_dl']}")
+				print(f"Protected Embed : {u['result'][0]['protected_embed']}")
+				print("#" * 40)
+				bot.delete_messages(message.chat.id,[smsg.message_id])
+				smsg = bot.send_message(message.chat.id, f"**Status :** {u['status']}\n\n**Video ID :** {u['result'][0]['filecode']}\n\n**Download Url :** {u['result'][0]['download_url']}\n\n**Protected DL :** {u['result'][0]['protected_dl']}\n\n**Protected Embed :** {u['result'][0]['protected_embed']}\n\n ", reply_to_message_id=message.message_id)
+			except: 
+			    pass
+		os.remove(file)
+		if os.path.exists(f'{message.chat.id}{message.message_id}upstatus.txt'): os.remove(f'{message.chat.id}{message.message_id}upstatus.txt')
+    
 
 
 @bot.on_message(filters.command('help') & filters.private)
