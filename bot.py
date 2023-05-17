ffd12353fa82e1dbde73bd0c283fab8973ee8332
AdityaRajJSK <73116892+AdityaRajJSK@users.noreply.github.com>
2023-05-17 16:06:16 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -219,7 +219,7 @@ def handle_private(message,chatid,msgid):
 		upsta = threading.Thread(target=lambda:upstatus(f'{message.chat.id}{message.message_id}upstatus.txt',smsg),daemon=True)
 		upsta.start()
 		
-		bot.edit_message_text(message.chat.id,[smsg.message_id], f"__Uploading Please Wait...__")
+		"""bot.edit_message_text(message.chat.id,[smsg.message_id], f"__Uploading Please Wait...__")"""
 		
 		path=file
 		if "Document" or "Video" in str(msg):
